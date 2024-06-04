0baa4b15ce4b2bd066780612de7e5f54cd6f3214
Ted2024 <ted27638178@gmail.com>
2024-06-04 14:50:40 +0800

    modify

---
+++ 2024_06_04/test.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("XXXXXX")
+
+
+
+windoe= Window()
+windoe.mainloop()

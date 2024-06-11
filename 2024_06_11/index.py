2c4c84759d5ed8ea829ab03703351cecc8cb21f7
TedTian <ted27638178@gmail.com>
2024-06-11 15:35:32 +0800

    commit

--- 2024_06_11/index.py
+++ 2024_06_11/index.py
@@ -5,12 +5,12 @@ from tkinter import messagebox
 from tools import CustomMessagebox
 
 class Window(ThemedTk):
-    def __init__(self,theme:str | None,**kwargs): #定義 init
+    def __init__(self,theme:str|None,**kwargs):
         super().__init__(**kwargs) #呼叫 init
-        self.title("BMI計算器")
-        #self.configure(bg="#D3D3D3")    # 改變背景顏色
-        #self.geometry("350x350+100+50") # 設置窗口初始大小
-        self.resizable(False,False )     # 允許窗口大小可調
+        self.title("BMI計算器")           
+        #self.configure(bg="#D3D3D3")    # 改變背景顏色大小
+        #self.geometry("350x350+100+50") # 設置窗口初始
+        self.resizable(False,False)      # 允許窗口大小可調
         style = ttk.Style()
         style.configure('input.TFrame',background='#ffffff')
         style.configure('press.TButton',font=('arial',10))
@@ -21,7 +21,6 @@ class Window(ThemedTk):
         titleFrame.pack(padx=100,pady=(0,20))
         #========================
         input_frame = ttk.Frame(self,style='Input.TFrame')
-
         # 姓名
         label_name = ttk.Label(input_frame, text="姓名:")
         label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
@@ -46,23 +45,20 @@ class Window(ThemedTk):
         self.weight_value = tk.StringVar()
         self.weight_value.set('')
         entry_weight = ttk.Entry(input_frame,textvariable=self.weight_value)
-        entry_weight.grid(row=2, column=1, padx=5, pady=5)
+        entry_weight.grid(row=2, column=1, padx=5, pady=5)    
 
-        
-        #視窗介面內部大小
-        input_frame.pack(padx=30,pady=10)
-        # =========================================================
+        input_frame.pack(pady=10,padx=30)
+        #===================================
 
-        # 計算按鈕設計
         button_calculate = ttk.Button(self, text="計算", command=self.show_bmi_result,style='press.TButton')
         button_calculate.pack(side=tk.RIGHT,padx=(0,35),pady=10)
 
-
+    
     def show_bmi_result(self):
         try:
-            name:str = self.entry_name.get()
-            height:int = int(self.entry_height.get())
-            weight:int = int(self.entry_weight.get())
+            name:str = self.name_value.get()
+            height:int = int(self.hight_value.get())
+            weight:int = int(self.weight_value.get())
         
         #except UnboundLocalError:
             #messagebox.showwarning("Warning","欄位沒有填寫")
@@ -72,7 +68,6 @@ class Window(ThemedTk):
             messagebox.showwarning("Warning","不知明的錯誤")
         else:
             self.show_result(name=name,height=height,weight=weight)
-
 
 
     def show_result(self,name:str,height:int,weight:int):
@@ -85,7 +80,7 @@ class Window(ThemedTk):
                 advice = f"您需要至少增加 {abs(weight_change):.2f} 公斤才能達到正常體重。"
             elif 18.5 <= bmi <= 24.9:
                 status = "正常"
-                status_color = "black"
+                status_color = "blue"
                 advice = "您的體重正常，請保持！"
             else:
                 status = "體重過重"
@@ -94,16 +89,16 @@ class Window(ThemedTk):
                 status_color = "red"
                 advice = f"您需要至少減少 {abs(weight_change):.2f} 公斤才能達到正常體重。"
 
-                CustomMessagebox(self,title="BMI",name=name,bmi=bmi,status=status,advice=advice)
-            
+            CustomMessagebox(self,title="BMI",name=name,bmi=bmi,status=status,advice=advice,status_color=status_color)
+
     def __repr__(self):
-        return "我是window的實體"        
-
-
-
+        return "我是window的實體"
+            
+            
+            
 
 def main():
-    window = Window(theme="arc") #最少需要一個套用theme
+    window = Window(theme='arc')
     window.mainloop()
 
 if __name__ == '__main__':

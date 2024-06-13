ed15beb05fa19c0ad4f06a2078ae0bc6d97fb482
TedTian <ted27638178@gmail.com>
2024-06-13 12:55:12 +0800

    commit

--- 類別/HW/issue235/index.py
+++ 類別/HW/issue235/index.py
@@ -1,4 +1,4 @@
-import data
+import 類別.HW.issue235.YouBike_data as YouBike_data
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
@@ -8,7 +8,7 @@ class Window(ThemedTk):
       def __init__(self,theme:str|None,**kwargs):
         super().__init__(**kwargs)
         try:
-            ubike:list[dict] = data.load_data()
+            ubike:list[dict] = YouBike_data.load_data()
         # except Exception as error:
         #     print(error) 
         except Exception:

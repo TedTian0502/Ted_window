7349f85619ac82b0b8c6d9cfb67dd31a083e2f0b
TedTian <ted27638178@gmail.com>
2024-06-17 16:53:25 +0800

    modify

--- 2024_06_17/index.py
+++ 2024_06_17/index.py
@@ -63,42 +63,46 @@ class Window(ThemedTk):
         scrollbar = ttk.Scrollbar(tableFrame, orient=tk.VERTICAL, command=tree.yview)
         tree.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1, sticky='ns')
-        tableFrame.pack(expand=True,fill=tk.BOTH,padx=20,pady=20)
+        tableFrame.pack(padx=20,pady=20)
         #======================================
         self.pieChartFrame = PieChartFrame(mainFrame)
-        self.pieChartFrame.pack(expand=True,fill='both')
-        mainFrame.pack(expand=True,fill=tk.BOTH,padx=10,pady=10)
+        self.pieChartFrame.pack()
+        mainFrame.pack(padx=10,pady=10)
 
     def item_selected(self,event):
         tree = event.widget
         records:list[list] = [] 
-        for selected_item in tree.selection():
+        for selected_item in tree.selection()[:3]: #[:3]代表只可以選取3個，多了也不會選取
             item = tree.item(selected_item)
             record:list = item['values']
             records.append(record)
         self.pieChartFrame.infos = records
     
 
-
-
-
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class PieChartFrame(ttk.Frame):
     def __init__(self,master:Misc,**kwargs):
         super().__init__(master=master,**kwargs)
         self.configure({'borderwidth':2,'relief':'groove'})
         #self.config({'borderwidth':2,'relief':'groove'})        
         #self['borderwidth'] = 2
-        #self['relief'] = 'groove' 
-        
+        #self['relief'] = 'groove'
+        style = ttk.Style()
+        style.configure('abc.TFrame',background='#ffffff')
+        self.configure(style='abc.TFrame')      
+
     @property
     def infos(self)->None:
-            return
-
+        return None
+    
+    
     @infos.setter
     def infos(self,datas:list[list]) -> None:
         for w in self.winfo_children():
-            w.destroy()
-            
+            w.destroy()        
+
         for data in datas:
             sitename:str = data[0]
             area:str = data[1]
@@ -107,17 +111,80 @@ class PieChartFrame(ttk.Frame):
             total:int = data[4]
             rents:int = data[5]
             returns:int = data[6]
-            oneFrame = ttk.Frame(self)
-            ttk.Label(oneFrame,text=area).grid(row=0,column=0)
-            oneFrame.pack(side='left') 
+            oneFrame = ttk.Frame(self,style='abc.TFrame')
+            ttk.Label(oneFrame,text="行政區:").grid(row=0,column=0,sticky='e')
+            ttk.Label(oneFrame,text=area).grid(row=0,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="站點名稱:").grid(row=1,column=0,sticky='e')
+            ttk.Label(oneFrame,text=sitename).grid(row=1,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="時間:").grid(row=2,column=0,sticky='e')
+            ttk.Label(oneFrame,text=info_time).grid(row=2,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="地址:").grid(row=3,column=0,sticky='e')
+            ttk.Label(oneFrame,text=address).grid(row=3,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="總車輛數:").grid(row=4,column=0,sticky='e')
+            ttk.Label(oneFrame,text=str(total)).grid(row=4,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="可借:").grid(row=5,column=0,sticky='e')
+            ttk.Label(oneFrame,text=str(rents)).grid(row=5,column=1,sticky='w')
+
+            ttk.Label(oneFrame,text="可還:").grid(row=6,column=0,sticky='e')
+            ttk.Label(oneFrame,text=str(returns)).grid(row=6,column=1,sticky='w')
+
+            def func(pct, allvals):
+                absolute = int(np.round(pct/100.*np.sum(allvals)))
+                return f"{absolute:d}pcs - {pct:.1f}%"
+
+            values = [rents, returns]
+            labels = ['Rend','Return']
+            colors = ['green','red']
+            figure = plt.figure(figsize=(5,5),dpi=72)
+            axes = figure.add_subplot()
+            axes.pie(values,colors=colors,
+                    labels=labels,
+                    labeldistance=1.2,
+                    shadow=True,
+                    autopct=lambda pct: func(pct, values),
+                    textprops=dict(color="white"))
+            
+            axes.legend(title="rate:",
+                        loc="center left",
+                        bbox_to_anchor=(0, 0, 0, 2))
+            
+            canvas = FigureCanvasTkAgg(figure,oneFrame)
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=7,column=0,columnspan=2)
+
+            #顯示後馬上消滅canvas,可以不會佔記憶體
+            for item in canvas.get_tk_widget().find_all():
+                canvas.get_tk_widget().delete(item)
+            #顯示後馬上消滅figure,可以不會佔記憶體
+            plt.close()
+            
+
+            oneFrame.pack(side='left',expand=True,fill='both')  
+
+
+
+
 
 
 
 
 
 def main():
+    def on_closing():
+        print("手動關閉視窗")
+        window.destroy()
+        window.quit()
+
+    
     window = Window(theme='breeze')
+    window.protocol("WM_DELETE_WINDOW", on_closing)    
     window.mainloop()
+
 
 if __name__ == '__main__':
     main()

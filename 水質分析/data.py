7fb56984310491c01705e7a9a3a81250a7badd95
Ted2024 <ted27638178@gmail.com>
2024-06-21 14:34:24 +0800

    modify

--- 水質分析/data.py
+++ 水質分析/data.py
@@ -15,41 +15,39 @@ def __download_json():
     
 
 class Info(BaseModel):
-    sna:str
-    sarea:str
-    mday:str
-    ar:str
-    act:str
-    updateTime:str
-    total:int
-    rent_bikes:int = Field(alias="available_rent_bikes")
-    lat:float = Field(alias="latitude")
-    lng:float = Field(alias="longitude")
-    retuen_bikes:int = Field(alias="available_return_bikes")
+    sitename:str #站點中文名稱
+    county:str   #所在縣市
+    basin:str    #河川流域
+    river:str    #河流名稱
+    sampledate:str #取樣日期和時間
+    itemname:str   #測量項目中文名稱
+    itemvalue:float #測量值
+    itemunit:str  #測量單位
+    
 
     model_config = ConfigDict(
         populate_by_name=True,
     )
 
-    @field_validator("sna",mode='before')
+    @field_validator("sitename",mode='before')
     @classmethod
     def flex_string(cls, value:str)->str:
         return value.split(sep="_")[-1]
 
-class Youbike_Data(RootModel):
+class Water_Data(RootModel):
     root:list[Info]
 
 def load_data()->list[dict]:
     all_data:dict[any] = __download_json()
-    youbike_data:Youbike_Data = Youbike_Data.model_validate(all_data)
-    data = youbike_data.model_dump()
+    Water_Data_data:Water_Data = Water_Data.model_validate(all_data)
+    data = Water_Data_data.model_dump()
     return data
 
 
 
 class FilterData(object):
     @staticmethod
-    def get_selected_coordinate(sna:str,data:list[dict]) -> Info:    
-        right_list:list[dict] = list(filter(lambda item:True if item['sna']==sna else False ,data))
+    def get_selected_coordinate(sitename:str,data:list[dict]) -> Info:    
+        right_list:list[dict] = list(filter(lambda item:True if item['sitename']==sitename else False ,data))
         data:dict = right_list[0]
         return Info.model_validate(data)
--- 水質分析/index.py
+++ 水質分析/index.py
@@ -1,190 +1,133 @@
-from ttkthemes import ThemedTk
 import tkinter as tk
-from tkinter import ttk, messagebox,Misc
-import data
-from data import FilterData,Info
-from tools import CustomMessagebox
-
-
-
-class Window(ThemedTk):
-    def __init__(self,theme:str='arc',**kwargs):
-        super().__init__(theme=theme,**kwargs)
-        self.title('台北市YouBike2.0及時資料')
-        try:
-            self.__data = data.load_data()
-        except Exception as e:
-            messagebox.showwarning(title='警告',message=str(e))
-        
-        self._display_interface()
-        
-    @property                   #呼叫執行Line11~19
-    def data(self)->list[dict]:
-        return self.__data
-    
-
-    def _display_interface(self):
-        mainFrame = ttk.Frame(borderwidth=1,relief='groove')
-        ttk.Label(mainFrame,text="台北市YouBike2.0及時資料",font=('arial',25)).pack(pady=(20,10))
-        #=================================
-        tableFrame = ttk.Frame(mainFrame)
-        columns = ('sna', 'sarea', 'mday','ar','total','rent_bikes','retuen_bikes')
-        tree = ttk.Treeview(tableFrame, columns=columns, show='headings')
-        # define headings
-        tree.heading('sna', text='站點')
-        tree.heading('sarea', text='行政區')
-        tree.heading('mday', text='時間')
-        tree.heading('ar', text='地址')
-        tree.heading('total', text='總數')
-        tree.heading('rent_bikes', text='可借')
-        tree.heading('retuen_bikes', text='可還')
-
-        # 定義欄位寬度
-        tree.column('sarea',width=70,anchor=tk.CENTER)
-        tree.column('mday',width=120,anchor=tk.CENTER)
-        tree.column('ar',minwidth=100)
-        tree.column('total',width=50,anchor=tk.CENTER)
-        tree.column('rent_bikes',width=50,anchor=tk.CENTER)
-        tree.column('retuen_bikes',width=50,anchor=tk.CENTER)
-
-        # bind使用者的事件
-        tree.bind('<<TreeviewSelect>>', self.item_selected)
-
-
-        
-        
-        # add data to the treeview
-        for site in self.data:
-            tree.insert('', tk.END,
-                        values=(site['sna'],site['sarea'],site['mday'],site['ar'],site['total'],site['rent_bikes'],site['retuen_bikes']))
-        
-        tree.grid(row=0, column=0, sticky='nsew')
-
-        scrollbar = ttk.Scrollbar(tableFrame, orient=tk.VERTICAL, command=tree.yview)
-        tree.configure(yscroll=scrollbar.set)
-        scrollbar.grid(row=0, column=1, sticky='ns')
-        tableFrame.pack(padx=20,pady=20)
-        #======================================
-        self.pieChartFrame = PieChartFrame(mainFrame)
-        self.pieChartFrame.pack()
-        mainFrame.pack(padx=10,pady=10)
-
-    def item_selected(self,event):
-        tree = event.widget
-        records:list[list] = [] 
-        for selected_item in tree.selection()[:3]: #[:3]代表只可以選取3個，多了也不會選取
-            item = tree.item(selected_item)
-            record:list = item['values']
-            records.append(record)
-        self.pieChartFrame.infos = records
-    
-
+from tkinter import ttk
+from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-class PieChartFrame(ttk.Frame):
-    def __init__(self,master:Misc,**kwargs):
-        super().__init__(master=master,**kwargs)
-        self.configure({'borderwidth':2,'relief':'groove'})
-        #self.config({'borderwidth':2,'relief':'groove'})        
-        #self['borderwidth'] = 2
-        #self['relief'] = 'groove'
-        style = ttk.Style()
-        style.configure('abc.TFrame',background='#ffffff')
-        self.configure(style='abc.TFrame')      
+import datasource
 
-    @property
-    def infos(self)->None:
-        return None
-    
-    
-    @infos.setter
-    def infos(self,datas:list[list]) -> None:
-        for w in self.winfo_children():
-            w.destroy()        
+filterWrose_numbers=3
 
-        for data in datas:
-            sitename:str = data[0]
-            area:str = data[1]
-            info_time:str = data[2]
-            address:str = data[3]
-            total:int = data[4]
-            rents:int = data[5]
-            returns:int = data[6]
-            oneFrame = ttk.Frame(self,style='abc.TFrame')
-            ttk.Label(oneFrame,text="行政區:").grid(row=0,column=0,sticky='e')
-            ttk.Label(oneFrame,text=area).grid(row=0,column=1,sticky='w')
+class MyWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-            ttk.Label(oneFrame,text="站點名稱:").grid(row=1,column=0,sticky='e')
-            ttk.Label(oneFrame,text=sitename).grid(row=1,column=1,sticky='w')
+        self.selectdata = None  # Placeholder for the data
 
-            ttk.Label(oneFrame,text="時間:").grid(row=2,column=0,sticky='e')
-            ttk.Label(oneFrame,text=info_time).grid(row=2,column=1,sticky='w')
+        # add menubar that contains a menu
+        self.menubar = tk.Menu(self)
+        self.config(menu=self.menubar)
+        # add command menu in menubar
+        self.command_menu = tk.Menu(self.menubar)
+        self.command_menu.add_command(label="設定", command=self.menu_setting_click)
+        self.command_menu.add_command(label="離開", command=self.destroy)
+        self.menubar.add_cascade(label="選項", menu=self.command_menu)
 
-            ttk.Label(oneFrame,text="地址:").grid(row=3,column=0,sticky='e')
-            ttk.Label(oneFrame,text=address).grid(row=3,column=1,sticky='w')
+        
+        # topFrame
+        self.topFrame = ttk.Labelframe(self)
+        self.topFrame.pack(side=tk.TOP)
 
-            ttk.Label(oneFrame,text="總車輛數:").grid(row=4,column=0,sticky='e')
-            ttk.Label(oneFrame,text=str(total)).grid(row=4,column=1,sticky='w')
+        # bottomFrame
+        self.bottomFrame = ttk.LabelFrame(self)
+        self.bottomFrame.pack()
 
-            ttk.Label(oneFrame,text="可借:").grid(row=5,column=0,sticky='e')
-            ttk.Label(oneFrame,text=str(rents)).grid(row=5,column=1,sticky='w')
+        # add pic
+        pic001Image1 = Image.open("./images/pic001.jpg")
+        self.pic001 = ImageTk.PhotoImage(pic001Image1)
+        self.canvas = tk.Canvas(self.topFrame, width=500, height=250)
+        self.canvas.pack()
+        self.canvas.create_image(0, 5, image=self.pic001, anchor='nw')
+        # add logo
+        logoImage = Image.open('./Images/co2logo.png')
+        resizeImage = logoImage.resize((89, 82), Image.LANCZOS)
+        self.logoTkimage = ImageTk.PhotoImage(resizeImage)
+        self.canvas.create_image(20, 30, image=self.logoTkimage, anchor='nw')
 
-            ttk.Label(oneFrame,text="可還:").grid(row=6,column=0,sticky='e')
-            ttk.Label(oneFrame,text=str(returns)).grid(row=6,column=1,sticky='w')
+        # Create the combobox
+        self.combobox_frame = ttk.Frame(self.topFrame)
+        self.combobox_frame.pack(pady=10)
+        self.combobox_label = ttk.Label(self.combobox_frame, text="Select Country:")
+        self.combobox_label.pack(side=tk.LEFT)
+        self.combobox = ttk.Combobox(self.combobox_frame, state="readonly")
+        self.combobox.pack(side=tk.LEFT)
 
-            def func(pct, allvals):
-                absolute = int(np.round(pct/100.*np.sum(allvals)))
-                return f"{absolute:d}pcs - {pct:.1f}%"
+        self.selectdata = datasource.getInfo()
 
-            values = [rents, returns]
-            labels = ['Rend','Return']
-            colors = ['green','red']
-            figure = plt.figure(figsize=(5,5),dpi=72)
-            axes = figure.add_subplot()
-            axes.pie(values,colors=colors,
-                    labels=labels,
-                    labeldistance=1.2,
-                    shadow=True,
-                    autopct=lambda pct: func(pct, values),
-                    textprops=dict(color="white"))
-            
-            axes.legend(title="rate:",
-                        loc="center left",
-                        bbox_to_anchor=(0, 0, 0, 2))
-            
-            canvas = FigureCanvasTkAgg(figure,oneFrame)
-            canvas.draw()
-            canvas.get_tk_widget().grid(row=7,column=0,columnspan=2)
+        country_list = self.selectdata['country'].unique().tolist()
+        country_list = sorted(country_list)
+        country_list.insert(0, '請選擇一個國家vvv')
+        self.combobox_values = tuple(country_list)
+        self.combobox['values'] = self.combobox_values
+        self.combobox.current(0)  # select the first country by default
+        self.combobox.bind("<<ComboboxSelected>>", self.update_treeview)
 
-            #顯示後馬上消滅canvas,可以不會佔記憶體
-            for item in canvas.get_tk_widget().find_all():
-                canvas.get_tk_widget().delete(item)
-            #顯示後馬上消滅figure,可以不會佔記憶體
-            plt.close()
-            
+        # Create the treeview
+        columns = ('#1', '#2', '#3', '#4', '#5', '#6')
+        self.tree = ttk.Treeview(self.bottomFrame, columns=columns, show='headings')
+        self.tree.heading('#1', text='year')
+        self.tree.column("#1", minwidth=0, width=80)
+        self.tree.heading('#2', text='co2')
+        self.tree.column("#2", minwidth=0, width=80)
+        self.tree.heading('#3', text='coal_co2')
+        self.tree.column("#3", minwidth=0, width=80)
+        self.tree.heading('#4', text='gas_co2')
+        self.tree.column("#4", minwidth=0, width=80)
+        self.tree.heading('#5', text='oil_co2')
+        self.tree.column("#5", minwidth=0, width=80)
+        self.tree.heading('#6', text='trade_co2')
+        self.tree.column("#6", minwidth=0, width=80)
 
-            oneFrame.pack(side='left',expand=True,fill='both')  
+        self.tree.pack(side=tk.LEFT)
+
+        for i in range(self.selectdata['country'].size):
+            self.tree.insert('', tk.END, values=[self.selectdata.iloc[i, 1], self.selectdata.iloc[i, 2], self.selectdata.iloc[i, 3],self.selectdata.iloc[i, 4], self.selectdata.iloc[i, 5], self.selectdata.iloc[i, 6]], tags=self.selectdata.index[i])
+
+        # add scrollbat ontreeview
+        scrollbar = ttk.Scrollbar(self.bottomFrame, command=self.tree.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.tree.configure(yscrollcommand=scrollbar.set)
 
 
+    def menu_setting_click(self):
+        pass
 
 
+    def update_treeview(self, event):
+        combobox = event.widget
+        selected_index = combobox.current()
+        selected_country = self.combobox_values[selected_index]
+        #self.filterWrose3 = datasource.filterWrose3(self.combobox_values,filterWrose_numbers)
+
+        if selected_country != '請選擇國家別vvv':
+            filtered_data = self.selectdata[self.selectdata['country'] == selected_country]
+            self.tree.delete(*self.tree.get_children())
+            for i in range(filtered_data.shape[0]):
+                self.tree.insert('', tk.END, values=[filtered_data.iloc[i, 1], filtered_data.iloc[i, 2], filtered_data.iloc[i, 3], filtered_data.iloc[i, 4], filtered_data.iloc[i, 5], filtered_data.iloc[i, 6]], tags=filtered_data.index[i])
+
+    def show_line_chart(self):
+        selected_country = self.combobox.get()
+        if selected_country != '請選擇國家別vvv':
+            filtered_data = self.selectdata[self.selectdata['country'] == selected_country]
+            years = filtered_data['year']
+            co2_values = filtered_data['co2']
+            plt.plot(years, co2_values)
+            plt.xlabel('Year')
+            plt.ylabel('CO2 Emissions')
+            plt.title(f'CO2 Emissions in {selected_country}')
+            plt.show()
 
 
+    def create_widgets(self):
+        # Create the plot button
+        self.plot_button = ttk.Button(self.combobox_frame, text="查看線圖", command=self.show_line_chart)
+        self.plot_button.pack(side=tk.RIGHT, padx=10)
 
-
-
-def main():
-    def on_closing():
-        print("手動關閉視窗")
-        window.destroy()
-        window.quit()
-
-    
-    window = Window(theme='breeze')
-    window.protocol("WM_DELETE_WINDOW", on_closing)    
-    window.mainloop()
+    def run(self):
+        self.title("Co2 Data")
+        self.geometry("700x600")
+        self.create_widgets()
+        self.mainloop()
 
 
 if __name__ == '__main__':
-    main()
+    window = MyWindow()
+    window.run()

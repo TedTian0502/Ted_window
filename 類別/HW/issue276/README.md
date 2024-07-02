db0f76ced251ff1521e5ce1889e581b8ee933352
Ted2024 <ted27638178@gmail.com>
2024-07-02 10:28:56 +0800

    commit

---
+++ 類別/HW/issue276/README.md
@@ -0,0 +1,46 @@
+## issue276. 下載youbike的資料,進入postgreSQL的youbike的table
+
+### [程式碼]()
+
+---
+
+### postgreSQL
+
+select count(*) as 數量
+from youbike
+
+select *
+from youbike
+
+drop table if exists youbike;
+
+CREATE TABLE IF NOT EXISTS youbike (
+            _id Serial Primary Key,
+            sna VARCHAR(50) NOT NULL ,
+            sarea VARCHAR(50),
+            ar VARCHAR(100),
+            mday timestamp,
+            updateTime timestamp ,
+            total SMALLINT,
+            rent_bikes SMALLINT,
+            return_bikes SMALLINT,
+            lat REAL,
+            lng REAL,
+            act boolean,
+			UNIQUE(sna,updateTime));
+
+INSERT INTO youbike(sna,sarea,ar,mday,updatetime,total,rent_bikes,return_bikes,lat,lng,act)
+            VALUES ('YouBike2.0_捷運科技大樓站',
+			'大安區',
+			'全家旁邊',
+			'2035-03-01',
+			'2024-07-02 09:37:20',
+			0,0,0,0,0,true)
+			ON CONFLICT (sna,updateTime)
+			DO NOTHING;
+
+---
+
+### pgAdmin執行成功畫面
+
+![picture](./0702.png)
--- 類別/HW/issue276/index.py
+++ 類別/HW/issue276/index.py
@@ -20,33 +20,36 @@ def main():
             lat REAL,
             lng REAL,
             act boolean,
-            UNIQUE(sna, updateTime)  -- Add unique constraint on sna and updateTime
+            UNIQUE(sna,updateTime)
             );
             '''
             cursor.execute(sql)
 
-        all_data = data.load_data()
+        all_data:list[dict] = data.load_data()
 
         with conn.cursor() as cursor:
             insert_sql='''
-            INSERT INTO youbike(sna,sarea,ar,mday,updateTime,total,rent_bikes,return_bikes,lat,lng,act)
+            INSERT INTO youbike(sna,sarea,ar,mday,updatetime,total,rent_bikes,return_bikes,lat,lng,act)
             VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-            ON CONFLICT (sna, updateTime) DO NOTHING;  -- Handle conflict with DO NOTHING
+            ON CONFLICT (sna, updateTime)
+			DO NOTHING;
             '''
             for site in all_data:
                 cursor.execute(insert_sql,(site['sna'],
-                                           site['sarea'],
-                                           site['ar'],
-                                           site['mday'],
-                                           site['updateTime'],
-                                           site['total'],
-                                           site['rent_bikes'],
-                                           site['return_bikes'],  # Fixed typo 'retuen_bikes' to 'return_bikes'
-                                           site['lat'],
-                                           site['lng'],
-                                           site['act']
-                                           ))     
+                                        site['sarea'],
+                                        site['ar'],
+                                        site['mday'],
+                                        site['updateTime'],
+                                        site['total'],
+                                        site['rent_bikes'],
+                                        site['retuen_bikes'],
+                                        site['lat'],
+                                        site['lng'],
+                                        site['act']
+                                        ))     
     conn.close()
+
+
 
 if __name__ == '__main__':
     main()

16be8e474bc7e91c7a4484e3890f5580eaa2eb6c
TedTian <ted27638178@gmail.com>
2024-07-22 01:26:53 +0800

    commit

--- 類別/HW/波士頓房價預測_09_田恭豪/window.py
+++ 類別/HW/波士頓房價預測_09_田恭豪/window.py
@@ -9,9 +9,11 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from PIL import Image, ImageTk
 import chart.analysis as analysis
 import chart.analysis2 as analysis2
-
-
-# cd 波士頓房價預測
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+# ==================================================================
 
 # 使用 getInfo 函數從 dataset.py 中載入資料集
 df = getInfo()
@@ -60,7 +62,7 @@ class MyWindow(tk.Tk):
         self.label.pack(side="left")
 
         # combobox設計
-        self.combobox = ttk.Combobox(self.frame, values=["數據一", "數據二", "數據三"], state="readonly")
+        self.combobox = ttk.Combobox(self.frame, values=["資料前處理", "特徵變數"], state="readonly")
         self.combobox.set("請選擇圖表:")
         self.combobox.pack(side="left", padx=(5, 0))
 
@@ -81,12 +83,12 @@ class MyWindow(tk.Tk):
         self.background_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
 
         # 設置右側文字框架
-        self.right_frame = tk.Frame(self, bg="lightgrey", padx=10, pady=10)
-        self.right_frame.pack(side="right", fill="y", padx=5, pady=5, ipadx=10)
+        # self.right_frame = tk.Frame(self, bg="lightgrey", padx=10, pady=10)
+        # self.right_frame.pack(side="right", fill="y", padx=5, pady=5, ipadx=10)
 
         # 添加文字內容
-        self.info_label = tk.Label(self.right_frame, text="這裡是右側的文字內容區域。", bg="lightgrey", font=('Arial', 12))
-        self.info_label.pack(pady=10)
+        # self.info_label = tk.Label(self.right_frame, text="這裡是右側的文字內容區域。", bg="lightgrey", font=('Arial', 12))
+        # self.info_label.pack(pady=10)
 
     def center_window(self, width=800, height=600):
         # 取得螢幕的寬度和高度
@@ -106,20 +108,19 @@ class MyWindow(tk.Tk):
             messagebox.showwarning("警告", "請先選擇一個選項")
             return
         
-        if selected_option == "數據一":
+        if selected_option == "資料前處理":
             self.show_data_one_window()
-        elif selected_option == "數據二":
+        elif selected_option == "特徵變數":
             self.show_data_window()
-        elif selected_option == "數據三":
-            self.show_additional_data_window()
+        # elif selected_option == "數據三": #如果有第三個選項
+        #     self.show_additional_data_window()
 
     def create_treeview1(self):
         self.destroy_treeview1()
 
         self.tree_frame1 = tk.Frame(self.background_frame)
-        self.tree_frame1.pack(pady=10, padx=10)
+        self.tree_frame1.pack(pady=10, padx=10, fill='both', expand=True)
 
-        # 新增treeview標籤1
         self.label1 = tk.Label(self.tree_frame1, text="資料集", padx=20)
         self.label1.pack(side="top")
 
@@ -138,16 +139,14 @@ class MyWindow(tk.Tk):
         hsb1.pack(side="bottom", fill="x")
 
         self.tree1.pack(side="left", fill="both", expand=True)
-        self.tree_frame1.grid_rowconfigure(0, weight=1)
-        self.tree_frame1.grid_columnconfigure(0, weight=1)
 
         try:
             df = pd.read_csv("train_dataset.csv")
             for index, row in df.head(20).iterrows():
                 data = tuple(row)
                 self.tree1.insert("", "end", values=data)
-        except FileNotFoundError:
-            print("找不到指定的 CSV 檔案。")
+        except Exception as e:
+            messagebox.showerror("錯誤", f"讀取 CSV 檔案失敗: {e}")
 
     def create_treeview2(self):
         self.destroy_treeview2()
@@ -209,15 +208,15 @@ class MyWindow(tk.Tk):
 
     def show_data_one_window(self):
         new_window = Toplevel(self)
-        new_window.title("數據一")
-        new_window.geometry("1250x400")
+        new_window.title("資料前處理")
+        new_window.geometry("1250x410")
 
         # 呼叫分析函數以顯示圖表
         analysis2.show_plots_in_window(new_window)
 
         try:
             # 圖表描述標籤
-            chart_label = tk.Label(new_window, text="123", font=('Tahoma', 15, 'bold'))
+            chart_label = tk.Label(new_window, text="確認是否有缺失值，查看盒鬚圖與房價變數最初的常態分佈", font=('Tahoma', 15, 'bold'))
             chart_label.pack(side=tk.BOTTOM, pady=5)    
 
         except FileNotFoundError as e:
@@ -225,29 +224,160 @@ class MyWindow(tk.Tk):
 
     def show_data_window(self):
         new_window = tk.Toplevel(self)
-        new_window.title("數據二")
-        new_window.geometry("650x450")
+        new_window.title("特徵變數")
+        new_window.geometry("1000x620")
 
-        try:
-            # 圖表描述標籤
-            chart_label = tk.Label(new_window, text="圖表二", font=('Tahoma', 15, 'bold'))
-            chart_label.pack(side=tk.TOP, pady=5)    
+        # 創建左邊框架
+        left_frame = tk.Frame(new_window, bg='lightgrey')
+        left_frame.grid(row=0, column=0, sticky='nsew')
 
-        except FileNotFoundError as e:
-            messagebox.showerror("錯誤", f"找不到指定的圖片檔案：{e}")
+        # 創建右邊框架
+        right_frame = tk.Frame(new_window, bg='#7D8ABC')
+        right_frame.grid(row=0, column=1, sticky='nsew')
 
-    def show_additional_data_window(self):
-        new_window = tk.Toplevel(self)
-        new_window.title("數據三")
-        new_window.geometry("850x420")
+        # 設定視窗內的行和列的權重
+        new_window.grid_rowconfigure(0, weight=1)
+        new_window.grid_columnconfigure(0, weight=1)
+        new_window.grid_columnconfigure(1, weight=1)
 
-        try:
-            # 圖表描述標籤
-            chart_label = tk.Label(new_window, text="圖表三", font=('Tahoma', 15, 'bold'))
-            chart_label.pack(side=tk.TOP, pady=5)
+        # 創建左上角的框架
+        upper_left_frame = tk.Frame(left_frame, bg='lightgrey')
+        upper_left_frame.pack(padx=10, anchor='center', fill='x')
 
-        except FileNotFoundError as e:
-            messagebox.showerror("錯誤", f"找不到指定的圖片檔案：{e}")
+        # 創建內部框架
+        inner_frame = tk.Frame(upper_left_frame, bg='lightgrey')
+        inner_frame.pack(padx=10, pady=10, anchor='center')
+
+        # 添加關閉按鈕
+        close_button = tk.Button(inner_frame, text="關閉", command=new_window.destroy, font=('Arial', 10))
+        close_button.pack(side='left', padx=5)
+
+        # 添加閾值標籤
+        threshold_label = tk.Label(inner_frame, text="閾值:", bg='yellow', font=('Arial', 14))
+        threshold_label.pack(side='left', padx=10)
+
+        # 添加閾值輸入框，設定預設提示文字
+        threshold_entry = tk.Entry(inner_frame, font=('Arial', 14), width=10)
+        threshold_entry.insert(0, "0.4")  # 設定預設為標準閾值:0.4
+        threshold_entry.pack(side='left', padx=10)
+
+        # 創建用於顯示選擇特徵的標籤
+        result_label = tk.Label(right_frame, text="", bg='#7D8ABC', fg='white', font=('Arial', 12), justify='left')
+        result_label.pack(padx=20, pady=20, anchor='nw')
+
+        # 創建右下角框架用於顯示回歸評估結果
+        eval_frame = tk.Frame(right_frame, bg='#508C9B')
+        eval_frame.pack(fill='both', expand=True)
+
+        # 創建用於顯示回歸評估結果的標籤
+        eval_label = tk.Label(eval_frame, text="", bg='#508C9B', fg='white', font=('Arial', 12), justify='left')
+        eval_label.pack(padx=20, pady=20, anchor='nw')
+
+        # 定義查看選項的函數
+        def view_options():
+            try:
+                # 嘗試讀取CSV檔案
+                data = pd.read_csv('train_dataset.csv')
+            except FileNotFoundError:
+                messagebox.showerror("錯誤", "找不到檔案: train_dataset.csv")  # 檔案未找到的錯誤
+                return
+            except Exception as e:
+                messagebox.showerror("錯誤", f"發生錯誤: {e}")  # 其他錯誤
+                return
+
+            # 清除之前的圖表
+            for widget in lower_left_frame.winfo_children():
+                widget.destroy()
+
+            # 創建相關性熱圖
+            plt.figure(figsize=(5, 3))
+            featuresCorr = data.corr()
+            ax = sns.heatmap(featuresCorr, annot=True, fmt=".2f", annot_kws={"size": 6}, cmap='coolwarm', cbar_kws={'shrink': .8})
+            plt.title('Features Correlation Heatmap', fontsize=8)
+            plt.xlabel('Features', fontsize=8)
+            plt.ylabel('Features', fontsize=8)
+            plt.xticks(fontsize=6)
+            plt.yticks(fontsize=6)
+
+            # 在Tkinter窗口中顯示圖表
+            canvas = FigureCanvasTkAgg(plt.gcf(), master=lower_left_frame)
+            canvas.draw()
+            canvas.get_tk_widget().pack(side='top', fill='both', expand=True, padx=5, pady=(0,5))
+
+            # 根據輸入的閾值x計算選擇特徵
+            try:
+                x_text = threshold_entry.get()
+                if x_text == "請輸入數值" or not x_text:
+                    messagebox.showerror("錯誤", "請輸入有效的數值")
+                    return
+                x = float(x_text)
+            except ValueError:
+                messagebox.showerror("錯誤", "請輸入有效的數值")
+                return
+
+            targetCorr = featuresCorr['PRICE']
+            targetCorr = targetCorr.drop('PRICE')
+            selectedFeatures = targetCorr[abs(targetCorr) > x]
+
+            result_text = f"選擇特徵數： {len(selectedFeatures)} \n選擇特徵:\n{selectedFeatures}"
+            result_label.config(text=result_text)
+
+            # 如果有選擇特徵，進行回歸分析
+            if not selectedFeatures.empty:
+                x = data[selectedFeatures.index]
+                y = data['PRICE']
+                x_train, x_test, y_train, y_test = train_test_split(
+                    x, y, test_size=0.2, random_state=1)  # R^2確認範圍，再調整random_state
+
+                # 數據標準化
+                transfer = StandardScaler()
+                x_train = transfer.fit_transform(x_train)
+                x_test = transfer.transform(x_test)
+
+                # 使用線性回歸模型
+                estimator = LinearRegression()
+                estimator.fit(x_train, y_train)
+                y_pred = estimator.predict(x_test)
+
+                # 評估回歸模型
+                mse = mean_squared_error(y_test, y_pred)
+                r2 = r2_score(y_test, y_pred)
+
+                eval_text = f"Mean Squared Error(MSE): {mse}\nR-squared(R^2): {r2}"
+                eval_label.config(text=eval_text)
+            else:
+                eval_label.config(text="沒有選擇特徵進行回歸分析")
+
+        # 添加查看選項按鈕
+        view_button = tk.Button(inner_frame, text="查看選項", command=view_options, font=('Arial', 10))
+        view_button.pack(side='left', padx=5)
+
+        # 創建左下角的框架
+        lower_left_frame = tk.Frame(left_frame, bg='lightgrey')
+        lower_left_frame.pack(padx=10, pady=5, anchor='nw', fill='both', expand=True)
+
+        # 創建右上角的框架
+        upper_right_frame = tk.Frame(right_frame, bg='#7D8ABC')
+        upper_right_frame.pack(padx=20, pady=10, anchor='nw', fill='x')
+
+        # 在右上角框架添加標籤
+        label_right = tk.Label(upper_right_frame, bg='#7D8ABC', fg='white', font=('Arial', 16))
+        label_right.pack(pady=50)
+
+        view_options()
+
+    # def show_additional_data_window(self):
+    #     new_window = tk.Toplevel(self)
+    #     new_window.title("數據三")
+    #     new_window.geometry("850x420")
+
+    #     try:
+    #         # 圖表描述標籤
+    #         chart_label = tk.Label(new_window, text="圖表三", font=('Tahoma', 15, 'bold'))
+    #         chart_label.pack(side=tk.TOP, pady=5)
+
+    #     except FileNotFoundError as e:
+    #         messagebox.showerror("錯誤", f"找不到指定的圖片檔案：{e}")
 
     def on_close(self):
         if messagebox.askokcancel("退出", "確定要退出嗎？"):

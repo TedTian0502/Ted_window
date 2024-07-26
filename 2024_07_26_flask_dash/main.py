5dee3a156c47c7d60f57dfd3ade6e284b73e0085
Ted2024 <ted27638178@gmail.com>
2024-07-26 10:43:02 +0800

    commit

--- 2024_07_26_flask_dash/main.py
+++ 2024_07_26_flask_dash/main.py
@@ -3,11 +3,11 @@ import data
 
 app = Flask(__name__)
 @app.route("/")
-def index1():
-    return render_template("index1.html.jinja")
-
-@app.route("/index1")
 def index():
+    return render_template("index.html.jinja")
+
+@app.route("/index1") #路徑
+def index1():
     # print(list(map(lambda value:value[0],data.get_areas())))
     selected_area = request.args.get('area')
     areas = (tup[0] for tup in data.get_areas())
@@ -18,4 +18,4 @@ def index():
     # area-->所有行政區
     # show_area-->要顯示的行政區
     # detail_snaes-->該行政區所有站點資訊
-    return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)
+    return render_template('index1.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)
